50179f53bbdb608e05cffa821c3521d4b6ace2ee
Eli Harel <eliharel3@gmail.com>
2025-05-09 11:40:55 -0600

    Create basic page and css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Home from "./pages/Home";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import PagesNavBar from "./components/PagesNavBar/PagesNavBar";
+
 function App() {
-  return <div>Traveler</div>;
+  return (
+    <BrowserRouter>
+      <PagesNavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
