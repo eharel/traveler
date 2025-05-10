2bd52cd6cacea54541a53a9d02cb8cb9853fdd1f
Eli Harel <eliharel3@gmail.com>
2025-05-10 14:13:32 -0600

    Add cities list

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,52 @@ import PagesNavBar from "./components/NavComponents/PagesNavBar/PagesNavBar";
 import Login from "./pages/Login";
 
 import "./index.css";
+import CityList from "./components/CityComponents/CityList/CityList";
+import { useEffect, useState } from "react";
+
+const URL = "http://localhost:9000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`${URL}/cities`);
+        const data = await response.json();
+        setCities(data);
+        setIsLoading(false);
+      } catch (error) {
+        // console.error("Error fetching cities:", error);
+        alert("Error fetching cities:", error);
+        setIsLoading(false);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <div style={{ backgroundColor: "var(--color-bg--main)" }}>
       <BrowserRouter>
         <PagesNavBar />
         <Routes>
           <Route index element={<Home />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>INDEX LIST</p>} />
-            <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/app" element={<AppLayout />}>
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
             <Route path="countries" element={<p>List of countries</p>} />
             <Route path="form" element={<p>Form</p>} />
           </Route>
